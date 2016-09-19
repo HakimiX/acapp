87808f1d42dd49e82c31635fe354de27b60451fd
Mustafa Hakimi <mustafahakimi@Mustafas-MacBook-Pro.local>
2016-09-19 19:01:53 +0200

    asdas

--- app/views.py
+++ app/views.py
@@ -38,7 +38,7 @@ messenger=MessengerClient('EAAX0hWUCaMcBANvFrF3y2YBP0rgrISZB2PeBwTxbJPOA3NaxZByn
 # a list of greeetings
 
 greeetings=['Hey', 'hey', 'hi','Hi', 'Hello', 'hello','Whats up']
-chile=['chile','Chile','Santiago', 'santiago']
+chile=['chile' 'vegan in chile', 'vegan in Chile','Vegan in Chile','Vegan in chile','Chile','Santiago', 'santiago']
 
 #cities=['copenhagen', 'Copenhagen', 'Aalborg', 'aalborg', 'Mimice', 'mimice', 'Dunakeszi', 'dunakeszi', 'Aarhus', 'aarhus', 'København', 'københavn', 'Budapest', 'budapest']
 
@@ -83,7 +83,7 @@ def webhook(request):
             if message in greeetings:
                 sendWelcome(recipient)
             if message in chile:
-                sendChile(recipient)    
+                sendChile(recipient)
             else:
                 call_apiai(message, recipient)
 
@@ -150,14 +150,10 @@ def sendWelcome(recipient):
 #Chile & Santiago easter egg
 def sendChile(recipient):
     msg="chile"
-    #attachment=attachments.ImageAttachment(url='https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAcwAAAAJGM1OGE5YmJkLTg2ZTUtNGE3Yi05OTg3LWRlZjhlZmViYzUwZg.png')
     message1=messages.Message(text=msg)
     request=messages.MessageRequest(recipient, message1)
     messenger.send(request)
 
-    #message2 = messages.Message(attachment=attachment)
-    #request = messages.MessageRequest(recipient, message2)
-    #messenger.send(request)
 
 #this function sends a Carousel to  facebook
 
